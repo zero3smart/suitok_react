288493e791d883ee295d6dd85287310abf83da60
Mikel <mg@suitok.co.uk>
2019-04-16 23:05:20 +0200

    mikel3

---
+++ build1/precache-manifest.12be0b9f53a739a70e3aaded629cb5c7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "89014af662b4ffe30f22d8b5e3b9d807",
+    "url": "/static/media/bk_home_women_portrait.89014af6.png"
+  },
+  {
+    "revision": "6a14a5f288cf4dbab346",
+    "url": "/static/css/main.5129d5ce.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "938ad479abc2bfeb0b83",
+    "url": "/static/js/2.938ad479.chunk.js"
+  },
+  {
+    "revision": "3534eb1af247035c5b80d6a042be3cd7",
+    "url": "/static/media/ic_logo.3534eb1a.png"
+  },
+  {
+    "revision": "d8eb4defc9240645e19b4e6e669fab6e",
+    "url": "/static/media/bk_home_women.d8eb4def.png"
+  },
+  {
+    "revision": "37bc52c4a4aae96035771ddbd699d95e",
+    "url": "/static/media/bk_home_men.37bc52c4.png"
+  },
+  {
+    "revision": "b7c7a581bb8d7fe8f78e777bb5e07ed4",
+    "url": "/static/media/bk_home_kids.b7c7a581.png"
+  },
+  {
+    "revision": "ed1714231a7d12897e04c03bea51ff67",
+    "url": "/static/media/bv_home_women.ed171423.mp4"
+  },
+  {
+    "revision": "6a14a5f288cf4dbab346",
+    "url": "/static/js/main.6a14a5f2.chunk.js"
+  },
+  {
+    "revision": "36bca023aba8fab197ef29afad18c59b",
+    "url": "/static/media/bv_home_men.36bca023.mp4"
+  },
+  {
+    "revision": "e6b3f4529ccb8f0ab079629216bd9638",
+    "url": "/static/media/bk_home_men_portrait.e6b3f452.png"
+  },
+  {
+    "revision": "f522a68b8d719899aa6e52a941e398d5",
+    "url": "/static/media/bv_home_kids.f522a68b.mp4"
+  },
+  {
+    "revision": "86f3f856b70726fa3d0032320ccf2f5e",
+    "url": "/static/media/bk_home_kids_portrait.86f3f856.png"
+  },
+  {
+    "revision": "47929f3873f2d817520bc0c56ace2f1f",
+    "url": "/static/media/ic_browser_safari.47929f38.png"
+  },
+  {
+    "revision": "f2e6ef431ae62da8f56bbd90442af154",
+    "url": "/static/media/ProximaNova-Semibold.f2e6ef43.woff"
+  },
+  {
+    "revision": "37c2dbc0925515c36143ed13d4d8acd7",
+    "url": "/static/media/ProximaNova-Regular.37c2dbc0.woff"
+  },
+  {
+    "revision": "4150441863d03a60e5dc61d673355018",
+    "url": "/index.html"
+  }
+];
